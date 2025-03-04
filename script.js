7ec8ff5dedb02c82245db981f80e68796f3c9c87
Arth370 <arthurqunilato@gmail.com>
2025-03-03 22:51:57 -0300

    contato

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const zap = document.querySelector('#zap')
+const git = document.querySelector('#git')
+const link = document.querySelector('#link')
+const insta = document.querySelector('#insta')
+const contatoBotao = document.querySelector('#contatao')
+const ContatoImg = document.querySelector('#contImg')
+const numero = document.querySelector('#num')
+console.log(contatoBotao)
+zap.addEventListener('click',()=>{
+    ContatoImg.src='./imgs/logos/zapbranco.png'
+    numero.textContent='XX XXXXX-XXXX'
+    contatoBotao.classList.add('contato')
+    contatoBotao.href='#contato'
+})
+git.addEventListener('click',()=>{
+    contatoBotao.href='https://github.com/Arth370'
+    ContatoImg.src='./imgs/logos/gitbranco.png'
+    numero.textContent='Arth370'
+    contatoBotao.classList.add('contato')
+})
+link.addEventListener('click',()=>{
+    contatoBotao.href='https://www.linkedin.com/in/arthur-henrique-38872234a/'
+    ContatoImg.src='./imgs/logos/linkbranco.png'
+    numero.textContent='Arthur Henrique'
+    contatoBotao.classList.add('contato')
+})
+insta.addEventListener('click',()=>{
+    contatoBotao.href='https://www.instagram.com/arth_xhju/'
+    ContatoImg.src='./imgs/logos/instabranco.png'
+    numero.textContent='Arthur Henrique'
+    contatoBotao.classList.add('contato')
+})
+
+
+
 const flechaL=document.querySelector('#ArrowL')
 const flechaD=document.querySelector('#ArrowD')
 const img = document.querySelector('#Imga')
